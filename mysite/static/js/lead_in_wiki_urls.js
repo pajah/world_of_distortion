d015c6075efc5044444d6c535552ad98c4af34cd
pajah <pasha.rebrov@gmail.com>
2018-07-12 23:50:18 +0300

    fix js script

--- mysite/static/js/lead_in_wiki_urls.js
+++ mysite/static/js/lead_in_wiki_urls.js
@@ -1,2 +1,2 @@
 jQuery(".lead_in a").attr("onclick", "window.open(this.href, 'windowName', 'width=1000, height=700, left=24, top=24, scrollbars, resizable'); return false;");
-jQuery(".lead_in a").setAttribute("target", "_blank");
+jQuery(".lead_in a").attr("target", "_blank");
