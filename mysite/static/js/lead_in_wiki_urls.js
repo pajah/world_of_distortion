94aaf0ca136f1f0487aab0a4f5bac5a9de95dcf3
pajah <pasha.rebrov@gmail.com>
2018-07-12 23:34:02 +0300

    style

---
+++ mysite/static/js/lead_in_wiki_urls.js
@@ -0,0 +1,2 @@
+$(".lead_in a").attr("onclick", "window.open(this.href, 'windowName', 'width=1000, height=700, left=24, top=24, scrollbars, resizable'); return false;");
+$(".lead_in a").setAttribute("target", "_blank");
